75304e0f9c237240eb721a225bafe1880b2aacf5
Thomas Gillespie <tomjamesgillespie@gmail.com>
2012-06-15 20:14:42 +0100

    Fix/test memory manager

--- build.bat
+++ build.bat
@@ -5,8 +5,9 @@ set CFLAGS=-Wall --std=c99 -O2 -fdata-sections -ffunction-sections
 "%PREFIX%as" -o build\gpio.o gpio.S
 "%PREFIX%as" -o build\mail.o mail.S
 "%PREFIX%as" -o build\memory.o memory.S
+"%PREFIX%as" -o build\fb.o fb.S
 
-"%PREFIX%ld" -e entry --gc-sections -o build\kernel.elf build\start.o build\gpio.o build\mail.o build\memory.o
+"%PREFIX%ld" -e entry --gc-sections -o build\kernel.elf build\start.o build\gpio.o build\mail.o build\memory.o build\fb.o
 "%PREFIX%strip" -s -x -X build\kernel.elf
 "%PREFIX%objcopy" --output-target binary build\kernel.elf build\kernel.img
 
