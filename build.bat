ab6a59636324b5f411a51d7f6196466c4610ebf5
Thomas Gillespie <tomjamesgillespie@gmail.com>
2012-06-15 03:15:43 +0100

    Fixup build system

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 set PREFIX=arm-none-eabi-
 set CFLAGS=-Wall --std=c99 -O2 -fdata-sections -ffunction-sections
 
-"%PREFIX%as" -o start.o start.S
-"%PREFIX%as" -o gpio.o gpio.S
-"%PREFIX%as" -o mail.o mail.S
+"%PREFIX%as" -o build\start.o start.S
+"%PREFIX%as" -o build\gpio.o gpio.S
+"%PREFIX%as" -o build\mail.o mail.S
 
-"%PREFIX%ld" -e entry --gc-sections -o kernel.elf start.o gpio.o mail.o
-"%PREFIX%strip" -s -x -X kernel.elf
-"%PREFIX%objcopy" --output-target binary kernel.elf kernel.img
+"%PREFIX%ld" -e entry --gc-sections -o build\kernel.elf build\start.o build\gpio.o build\mail.o
+"%PREFIX%strip" -s -x -X build\kernel.elf
+"%PREFIX%objcopy" --output-target binary build\kernel.elf build\kernel.img
