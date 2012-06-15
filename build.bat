afad4fd75da5b24e82852b7910897ea4cf2ceace
Thomas Gillespie <tomjamesgillespie@gmail.com>
2012-06-15 19:28:48 +0100

    Memory manager + buxgfixes

--- build.bat
+++ build.bat
@@ -4,7 +4,10 @@ set CFLAGS=-Wall --std=c99 -O2 -fdata-sections -ffunction-sections
 "%PREFIX%as" -o build\start.o start.S
 "%PREFIX%as" -o build\gpio.o gpio.S
 "%PREFIX%as" -o build\mail.o mail.S
+"%PREFIX%as" -o build\memory.o memory.S
 
-"%PREFIX%ld" -e entry --gc-sections -o build\kernel.elf build\start.o build\gpio.o build\mail.o
+"%PREFIX%ld" -e entry --gc-sections -o build\kernel.elf build\start.o build\gpio.o build\mail.o build\memory.o
 "%PREFIX%strip" -s -x -X build\kernel.elf
 "%PREFIX%objcopy" --output-target binary build\kernel.elf build\kernel.img
+
+copy /Y build\kernel.img K:\
